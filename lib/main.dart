5604ecbcc22178a811302430635e4bf9f85b8ba5
Chamalka20 <chamalkagunawardana@gmail.com>
2023-04-09 12:05:05 +0530

    up

--- lib/main.dart
+++ lib/main.dart
@@ -14,19 +14,16 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Travel app',
+      initialRoute: '/',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+       
         primarySwatch: Colors.blue,
       ),
-      home: const welcomePage(),
+      routes: {
+        '/welcomePage':(context)=>const welcomePage(),
+
+      },
+      
     );
   }
 }
