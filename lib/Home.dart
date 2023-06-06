19571105e2e22efd0f74ab163e446363f6f3f057
Chamalka20 <chamalkagunawardana@gmail.com>
2023-06-06 20:37:54 +0530

    up

--- lib/Home.dart
+++ lib/Home.dart
@@ -906,136 +906,49 @@ final payload3 = {
                   mainAxisAlignment: MainAxisAlignment.center,
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    Padding(
-                      padding: const EdgeInsets.only(left:10),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Container(
-                            margin: const EdgeInsets.only(right:35),
-                            child: Column(
-                              children: [
-                                Row(
-                                  children: [
-                                    Image.asset("assets/images/homeClick.png",width:20,height:20)
-                                  ],
-                                ),
-                                Row(
-                                  children: [
-                                    Text("Home", style: GoogleFonts.cabin(
-                                                    // ignore: prefer_const_constructors
-                                                    textStyle: TextStyle(
-                                                    color: Color.fromARGB(255, 0, 0, 0),
-                                                    fontSize: 12,
-                                                    fontWeight: FontWeight.bold,
-                                                                                    
-                                                    ) 
-                                                  ))
-                                  ],
-                            
-                                )
-                              ],
-                            
-                            ),
-                          ),
-                           Container(
-                             margin: const EdgeInsets.only(right:30),
-                             child: Column(
-                              children: [
-                                Row(
-                                  children: [
-                                    Image.asset("assets/images/search.png",width:20,height:20)
-                                  ],
-                                ),
-                                Row(
-                                  children: [
-                                    Text("Search",
-                                       style: GoogleFonts.cabin(
-                                                    // ignore: prefer_const_constructors
-                                                    textStyle: TextStyle(
-                                                    color: Color.fromARGB(255, 189, 188, 188),
-                                                    fontSize: 12,
-                                                    fontWeight: FontWeight.bold,
-                                                                                    
-                                                    ) 
-                                                  )
-                                    )
-                                  ],
-                           
-                                )
-                              ],
-                           
-                                                   ),
-                           ),
-                           Container(
-                             margin: const EdgeInsets.only(right:30),
-                             child: Column(
-                              children: [
-                                Row(
-                                  children: [
-                                    Image.asset("assets/images/map.png",width:20,height:20)
-                                  ],
-                                ),
-                                Row(
-                                  children: [
-                                    Text("Plan",
-                                       style: GoogleFonts.cabin(
-                                                    // ignore: prefer_const_constructors
-                                                    textStyle: TextStyle(
-                                                    color: Color.fromARGB(255, 189, 188, 188),
-                                                    fontSize: 12,
-                                                    fontWeight: FontWeight.bold,
-                                                                                    
-                                                    ) 
-                                                  )
-                                    )
-                                  ],
-                           
-                                )
-                              ],
-                           
-                                                   ),
-                           ),
-                           Container(
-                             margin: const EdgeInsets.only(right:30),
-                             child: Column(
-                              children: [
-                                Row(
-                                  children: [
-                                    Image.asset("assets/images/like.png",width:20,height:20)
-                                  ],
-                                ),
-                                Row(
-                                  children: [
-                                    Text("Favorite",
-                                       style: GoogleFonts.cabin(
-                                                    // ignore: prefer_const_constructors
-                                                    textStyle: TextStyle(
-                                                    color: Color.fromARGB(255, 189, 188, 188),
-                                                    fontSize: 12,
-                                                    fontWeight: FontWeight.bold,
-                                                                                    
-                                                    ) 
-                                                  )
-                                    )
-                                  ],
-                           
-                                )
-                              ],
-                           
-                                                   ),
-                           ),
-                           Column(
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Container(
+                          margin: const EdgeInsets.only(right:37),
+                          child: Column(
                             children: [
                               Row(
                                 children: [
-                                  Image.asset("assets/images/user.png",width:20,height:20)
+                                  Image.asset("assets/images/homeClick.png",width:20,height:20)
                                 ],
                               ),
                               Row(
                                 children: [
-                                  Text("Account",
+                                  Text("Home", style: GoogleFonts.cabin(
+                                                  // ignore: prefer_const_constructors
+                                                  textStyle: TextStyle(
+                                                  color: Color.fromARGB(255, 0, 0, 0),
+                                                  fontSize: 12,
+                                                  fontWeight: FontWeight.bold,
+                                                                                  
+                                                  ) 
+                                                ))
+                                ],
+                          
+                              )
+                            ],
+                          
+                          ),
+                        ),
+                         Container(
+                           margin: const EdgeInsets.only(right:37),
+                           child: Column(
+                            children: [
+                              Row(
+                                children: [
+                                  Image.asset("assets/images/search.png",width:20,height:20)
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  Text("Search",
                                      style: GoogleFonts.cabin(
                                                   // ignore: prefer_const_constructors
                                                   textStyle: TextStyle(
@@ -1047,14 +960,98 @@ final payload3 = {
                                                 )
                                   )
                                 ],
-                           
+                         
                               )
                             ],
-                           
+                         
+                                                 ),
+                         ),
+                         Container(
+                           margin: const EdgeInsets.only(right:37),
+                           child: Column(
+                            children: [
+                              Row(
+                                children: [
+                                  Image.asset("assets/images/map.png",width:20,height:20)
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  Text("Plan",
+                                     style: GoogleFonts.cabin(
+                                                  // ignore: prefer_const_constructors
+                                                  textStyle: TextStyle(
+                                                  color: Color.fromARGB(255, 189, 188, 188),
+                                                  fontSize: 12,
+                                                  fontWeight: FontWeight.bold,
+                                                                                  
+                                                  ) 
+                                                )
+                                  )
+                                ],
+                         
+                              )
+                            ],
+                         
+                                                 ),
+                         ),
+                         Container(
+                           margin: const EdgeInsets.only(right:37),
+                           child: Column(
+                            children: [
+                              Row(
+                                children: [
+                                  Image.asset("assets/images/like.png",width:20,height:20)
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  Text("Favorite",
+                                     style: GoogleFonts.cabin(
+                                                  // ignore: prefer_const_constructors
+                                                  textStyle: TextStyle(
+                                                  color: Color.fromARGB(255, 189, 188, 188),
+                                                  fontSize: 12,
+                                                  fontWeight: FontWeight.bold,
+                                                                                  
+                                                  ) 
+                                                )
+                                  )
+                                ],
+                         
+                              )
+                            ],
+                         
+                                                 ),
+                         ),
+                         Column(
+                          children: [
+                            Row(
+                              children: [
+                                Image.asset("assets/images/user.png",width:20,height:20)
+                              ],
                             ),
-                      
-                        ],
-                      ),
+                            Row(
+                              children: [
+                                Text("Account",
+                                   style: GoogleFonts.cabin(
+                                                // ignore: prefer_const_constructors
+                                                textStyle: TextStyle(
+                                                color: Color.fromARGB(255, 189, 188, 188),
+                                                fontSize: 12,
+                                                fontWeight: FontWeight.bold,
+                                                                                
+                                                ) 
+                                              )
+                                )
+                              ],
+                         
+                            )
+                          ],
+                         
+                          ),
+                    
+                      ],
                     )
                   ],
 
