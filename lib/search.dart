8cf8911986c572f6935729fea28b6a6e96b8efc0
Chamalka20 <chamalkagunawardana@gmail.com>
2023-06-10 08:25:31 +0530

    up

--- lib/search.dart
+++ lib/search.dart
@@ -16,6 +16,7 @@ class search extends StatefulWidget {
 
 class _searchState extends State<search> {
 
+  bool  isTextFieldClicked= false;
   
   
 
@@ -25,37 +26,62 @@ class _searchState extends State<search> {
        SafeArea(
         child:Column(
           children: [
-            Padding(
-              padding: EdgeInsets.only(left:13.0,top:20.0,bottom:10.0),
-              child: Row(
-                children: [
-                  Text("Search",
-                        style: GoogleFonts.nunito(
-                                    // ignore: prefer_const_constructors
-                                    textStyle: TextStyle(
-                                    color: const Color.fromARGB(255, 27, 27, 27),
-                                    fontSize: 32,
-                                    fontWeight: FontWeight.bold,
-                            
-                                    ) 
-                                  )
-                      
-                      ),
-                  
-                ]  
+            Visibility(
+              visible: !isTextFieldClicked,
+              child: Padding(
+                padding: const EdgeInsets.only(left:13.0,top:20.0,bottom:10.0),
+                child: Row(
+                  children: [
+                    Text("Search",
+                          style: GoogleFonts.nunito(
+                                      // ignore: prefer_const_constructors
+                                      textStyle: TextStyle(
+                                      color: const Color.fromARGB(255, 27, 27, 27),
+                                      fontSize: 32,
+                                      fontWeight: FontWeight.bold,
+                              
+                                      ) 
+                                    )
+                        
+                        ),
+                    
+                  ]  
+                ),
               ),
             ),
             Padding(
-              padding: const EdgeInsets.only(left:13,top:9),
+              padding: const EdgeInsets.only(left:4,top:9),
               child: Row(
             
                 children: [
+                   Visibility(
+                    visible: isTextFieldClicked,
+                     child: IconButton(
+                        icon: const Icon(Icons.arrow_back),
+                        color: const Color.fromARGB(255, 145, 144, 144),
+                        iconSize: 26,
+                        onPressed: () {
+                   
+                          Navigator.of(context).pushReplacement(customPageRoutes(
+                          child:const search()));
+                   
+                          // Handle back button press
+                        },
+                     ),
+                   ),
+                  isTextFieldClicked?
                   Container(
-                    width: 330,
+                    
+                    width: 290,
                     height: 35,
                     
                     child:  TextField(
                       
+                       onTap: () {
+                        setState(() {
+                          isTextFieldClicked = true;
+                        });
+                      },
                       decoration: InputDecoration(
                       filled: true,
                       fillColor:  Color.fromARGB(255, 240, 238, 238),
@@ -75,12 +101,50 @@ class _searchState extends State<search> {
                         borderRadius: BorderRadius.circular(19.0),
                         
                         ),
-                        contentPadding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 5.0),
+                        contentPadding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 5.0),
+                      ),
+                  
+                  
+                    ),
+                  ):
+                  Container(
+                    
+                    width: 330,
+                    height: 35,
+                    
+                    child:  TextField(
+                      
+                       onTap: () {
+                        setState(() {
+                          isTextFieldClicked = true;
+                        });
+                      },
+                      decoration: InputDecoration(
+                      filled: true,
+                      fillColor:  Color.fromARGB(255, 240, 238, 238),
+                      hintText: 'Search',
+                      prefixIcon: Icon(Icons.search),
+                      hintStyle: GoogleFonts.cabin(
+                                    // ignore: prefer_const_constructors
+                                    textStyle: TextStyle(
+                                    color: Color.fromARGB(255, 145, 144, 144),
+                                    fontSize: 17,
+                                    fontWeight:FontWeight.w400,
+                                    
+                                    ) 
+                                  ),
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide.none,
+                        borderRadius: BorderRadius.circular(19.0),
+                        
+                        ),
+                        contentPadding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 5.0),
                       ),
                   
                   
                     ),
                   )
+
             
                 ],
                    
@@ -88,27 +152,30 @@ class _searchState extends State<search> {
                   
               ),
             ),
-            Padding(
-               padding: const EdgeInsets.only(left:13,top:30),
-              child: Row(
-            
-                children: [
-                  Text("Your recent searches",
-                    style: GoogleFonts.cabin(
-                          // ignore: prefer_const_constructors
-                          textStyle: TextStyle(
-                          color: const Color.fromARGB(255, 27, 27, 27),
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                  
-                          ) 
-                        )
-                          
-                  
-                  ),
-            
-                ],
-            
+            Visibility(
+              visible: !isTextFieldClicked,
+              child: Padding(
+                 padding: const EdgeInsets.only(left:13,top:30),
+                child: Row(
+              
+                  children: [
+                    Text("Your recent searches",
+                      style: GoogleFonts.cabin(
+                            // ignore: prefer_const_constructors
+                            textStyle: TextStyle(
+                            color: const Color.fromARGB(255, 27, 27, 27),
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                    
+                            ) 
+                          )
+                            
+                    
+                    ),
+              
+                  ],
+              
+                ),
               ),
             )
           ],
