f186caacde49098ed918a5f6624ba1831558352d
Chamalka20 <chamalkagunawardana@gmail.com>
2023-06-14 11:29:17 +0530

    up

--- lib/navigationPage.dart
+++ lib/navigationPage.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:travelapp/fechApiData.dart';
 import 'package:travelapp/search.dart';
