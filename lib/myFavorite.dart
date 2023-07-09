6d9248468e86f3519fdbdb5e09af0234e365e832
Chamalka20 <chamalkagunawardana@gmail.com>
2023-07-09 07:53:20 +0530

    up

--- lib/myFavorite.dart
+++ lib/myFavorite.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
@@ -11,6 +12,11 @@ class myFavorite extends StatefulWidget {
 class _myFavoriteState extends State<myFavorite> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Column(
+        
+      ),
+
+    );
   }
 }
