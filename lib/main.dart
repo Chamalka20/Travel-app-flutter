2dfa72e7b030f9f488c53cf2832a7d777ca04786
Chamalka20 <chamalkagunawardana@gmail.com>
2023-08-21 20:02:20 +0530

    up

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 
 import 'dart:io';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'Welcomepage.dart';
